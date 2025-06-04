70335c4a9746982352967f41c722eda965a0385f
Tonxay <toninpaserd@gmail.com>
2025-06-04 23:39:45 +0700

    fix cors 2

--- cmd/local/main.go
+++ cmd/local/main.go
@@ -51,11 +51,7 @@ func main() {
 		log.Fatalf("❌ Failed to connect to DB: %v", err)
 	}
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
+		AllowOrigins: []string{"*"},
 	}))
 	// API Routes
 	// api := app.Group(os.Getenv("API_PREFIX"))
