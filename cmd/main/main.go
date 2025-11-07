9841334aefbd6165f19544293a8990564d1c407d
Tonxay inpaserd <toninpaserd@gmail.com>
2025-11-07 20:19:34 +0700

    test

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,7 +5,6 @@ import (
 	"go-api/internal/api"
 	"go-api/internal/config/middleware"
 	gormpkg "go-api/internal/pkg"
-	"go-api/internal/services/service"
 	"log"
 	"os"
 	"time"
@@ -59,6 +58,6 @@ func main() {
 		port = "8080"
 	}
 	// service.Uploadimage()
-	service.Uploadimage()
+	// service.Uploadimage()
 	log.Fatal(app.Listen(":" + port))
 }
